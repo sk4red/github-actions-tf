7a97c75300028062542580882d0ab23811dab612
Dave Furmidge <devopswithdave@gmail.com>
2022-01-26 20:03:43 +0000

    slight change

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,23 +17,23 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
-  location = "eastus2"
+resource "azurerm_resource_group" "rg-dowd" {
+  name     = "rg-dowd"
+  location = "uksouth"
 }
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
+  name                = "dowd-vnet"
   address_space       = ["192.168.0.0/16"]
-  location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
+  location            = "uksouth"
+  resource_group_name = azurerm_resource_group.rg-dowd.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
+  resource_group_name  = azurerm_resource_group.rg-dowd.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "192.168.0.0/24"
 }
